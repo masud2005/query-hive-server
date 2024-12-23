ce9c066a7845c4f3efea46a3ed844756d4078d92
masud2005 <masud20012005@gmail.com>
2024-12-23 18:12:39 +0600

    Specific Query ID APIs Created

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cckud.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -37,10 +37,18 @@ async function run() {
             res.send(result);
         });
 
-        // Get all queries
+        // Get all queries APIs
         app.get('/queries', async (req, res) => {
             const cursor = queryHive.find({});
             const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        // Get a query by id
+        app.get('/queries/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await queryHive.findOne(query);
             res.send(result);
         });
 
