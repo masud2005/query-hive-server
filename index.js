755b05929abf16e767639666ce96667cd7e78149
masud2005 <masud20012005@gmail.com>
2024-12-23 19:41:08 +0600

    Delete Specific Query in Database Functionality Added

--- index.js
+++ index.js
@@ -52,6 +52,14 @@ async function run() {
             res.send(result);
         });
 
+        // Delete a query by id
+        app.delete('/queries/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await queryHive.deleteOne(query);
+            res.send(result);
+        });
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
